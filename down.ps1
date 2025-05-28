52b51e62aa1a066fe9b5ef0b28688910697ca7fb
matilesiw <matilesiw@gmail.com>
2025-05-27 21:57:39 -0300

    Ignore scripts y borrado

--- down.ps1
+++
@@ -1,7 +0,0 @@
-docker-compose -f template-local.yml down
-
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "Contenedores detenidos correctamente."
-} else {
-    Write-Error "Hubo un error al detener los contenedores."
-}
